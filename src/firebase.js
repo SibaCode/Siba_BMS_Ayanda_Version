c069b8bc191ccc3dffd1f8adb95450cbb82f3205
SibahleMvubu <smvubu@iqbusiness.net>
2025-07-28 22:39:00 +0200

    firebase_2

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBosWTZV8wyGN_jIgPqPCogoy2bss-BhE4",
-  authDomain: "businessmanagmentsystem-c3389.firebaseapp.com",
-  projectId: "businessmanagmentsystem-c3389",
-  storageBucket: "businessmanagmentsystem-c3389.firebasestorage.app",
-  messagingSenderId: "581978849125",
-  appId: "1:581978849125:web:acc3cbd2e62be494e3ab8c",
-  measurementId: "G-C6L7752QG4"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBosWTZV8wyGN_jIgPqPCogoy2bss-BhE4",
+//   authDomain: "businessmanagmentsystem-c3389.firebaseapp.com",
+//   projectId: "businessmanagmentsystem-c3389",
+//   storageBucket: "businessmanagmentsystem-c3389.firebasestorage.app",
+//   messagingSenderId: "581978849125",
+//   appId: "1:581978849125:web:acc3cbd2e62be494e3ab8c",
+//   measurementId: "G-C6L7752QG4"
+// };
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDKKMW0y7_Vw_9yIwFN_YpcyepGbAtPRqE",
 //   authDomain: "bms-kat.firebaseapp.com",
@@ -26,6 +26,15 @@ const firebaseConfig = {
 //   appId: "1:126068664770:web:764ce5ca37ac03c71a4e5b",
 //   measurementId: "G-TVKYE2HYNK"
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyDptvgHOV2zZT904AaP2ByNr4IMamyK_r0",
+  authDomain: "bms-ayanda.firebaseapp.com",
+  projectId: "bms-ayanda",
+  storageBucket: "bms-ayanda.firebasestorage.app",
+  messagingSenderId: "488259839090",
+  appId: "1:488259839090:web:de4def1ae4c43f1e18ceeb",
+  measurementId: "G-M8KMMGV3XE"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
